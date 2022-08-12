5f4dcefead7461dc18a33faabdd0b3ed26abdc53
Filipe Cupini <88508043+FilipeCupini@users.noreply.github.com>
2022-08-12 00:50:51 -0300

    continuação estudos js

--- loops.js
+++ loops.js
@@ -7,22 +7,38 @@ const destinos = new Array(
 )
 const idadeComprador = 18
 const acompanhado = true
-const cidade = `Salvador`
-let contador = 0
+const cidade = `Sao Paulo`
 
 console.log(`\nDestinos possíveis: `)
 console.log(destinos)
-
 const comprarPassagem = idadeComprador >= 18 || acompanhado == true
 
-while (contador < destinos.length) {
-    if (destinos[contador] == cidade){
-        console.log(`O destino está disponível ${cidade}`)
-        break
+// let contador = 0
+let verificaDestino = false;
+
+// while (contador < destinos.length) {                        Com while
+//     if (destinos[contador] == cidade) {
+//         console.log(`${cidade} está disponível`)
+//         verificaDestino = true
+//         break
+//     } else {
+//         console.log(`Destino não disponível`)
+//     }
+//     contador++
+// }
+
+for(let i = 0; i < destinos.length; i++) {
+    if (destinos[i] == cidade) {
+        console.log(`${cidade} está disponível`)
+        verificaDestino = true        
     } else {
         console.log(`Destino não disponível`)
-    }
-        
-
-
+    }    
 }
+
+if(comprarPassagem && verificaDestino){
+    console.log(`Boa viagem!!`)
+} else {
+    console.log(`Desculpa, você não pode viajar.`)
+}
+
